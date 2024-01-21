a792e23911002ef17306ace8543c267a51003233
91am <killaek.info@gmail.com>
2024-01-21 16:26:30 +0100

    Crear entidad users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'nest_movies', // Nombre de la base de datos como se creo en sql
+    entities: [User], // Las entidades que creamos (por ejemplo la tabla usuarios es la entidad users)
+    synchronize: true,
+  }),
+  UsersModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
